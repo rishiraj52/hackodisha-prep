9d6d051f60621f7964a209c14ea9d2dba606178a
rishiraj52 <rajrishi0509@gmail.com>
2022-07-23 23:10:05 +0530

    Admin Login Page Added

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 // import CantHome from "./pages/canteen/Home";
-import Hero from "./pages/login-pages/heroPage";
+// import Hero from "./pages/login-pages/heroPage";
+import AdminLogin from "./pages/login-pages/admin_login";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    <Hero />
+    {/* <Hero /> */}
     {/* <CantHome /> */}
+    <AdminLogin />
   </React.StrictMode>
 );
 
